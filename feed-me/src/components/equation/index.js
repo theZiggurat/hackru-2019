d3588b02f51bb367d2967ae3097e21f67f99d4f9
unknown <frappyd@gmail.com>
2019-10-19 12:06:35 -0400

    skeleton

---
+++ feed-me/src/components/equation/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Mathjs from 'mathjs';
+
+export default class Equation extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        
+    }
+}
---
+++ feed-me/src/components/input/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Input extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        
+    }
+}
