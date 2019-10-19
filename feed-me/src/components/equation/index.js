135523a72a1c95002fa8226fc31e5d31c050ce5d
unknown <frappyd@gmail.com>
2019-10-19 13:06:59 -0400

    added list

--- feed-me/src/components/equation/index.js
+++ feed-me/src/components/equation/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import {Input} from 'antd';
+import {Card, Input} from 'antd';
 import Mathjs from 'mathjs';
+import './index.css';
+
+const Eqlist = ({list}) => (
+    <div className="list">
+        {
+            list.map((item, i) => 
+            <Card style={{ width: 300 }}>
+                <p>{item}</p>
+          </Card>,
+        )}
+    </div>
+);
 
 export default class Equation extends React.Component {
     state = {
+        eqlist: []
+    }
 
+    onEnter = (eq) => {
+        let {eqlist} = this.state;
+        let newlist = [...eqlist, eq];
+        this.setState({
+            eqlist: newlist
+        })
     }
 
     render() {
         return <div className="input">
-            <Input size="large" placeholder="Equation"/>
+            <Input size="large" placeholder="Equation" 
+                onPressEnter={this.onEnter}/>
+            <Eqlist list={this.state.eqlist}/>
+            
         </div>;
     }
 }
